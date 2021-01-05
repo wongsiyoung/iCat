1c5c7489542fe98495baff991787e38dcef294ec
wongsiyoung <876734643@qq.com>
2021-01-05 22:03:43 +0800

    合并yangjh分支

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,4 +1,5 @@
 //app.js
+const articles = require('data/data_articles.js');/////////////////////////////////
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
@@ -18,7 +19,6 @@ App({
         traceUser: true,
       })
     }
-    
   },
   getUserInfo:function(cb){
     var that = this
@@ -38,8 +38,48 @@ App({
       })
     }
   },
-  
   globalData:{
     userInfo:null
+  },
+    // -------------------------------------------
+    getArticleList(cb) {
+      var that = this;
+  
+      if (this.globalData.articleList) {
+        typeof cb == 'function' && cb(this.globalData.articleList);
+      } else {
+        let list = [];
+  
+        this.getLocalArticles(storage => {
+          // 本地缓存数据
+          for (var k in storage) {
+            list.push(storage[k]);
+          }
+        });
+  
+        // 本地假数据
+        list.push(...articles.articles);
+        that.globalData.articleList = list;
+        typeof cb == 'function' && cb(that.globalData.articleList)
+      }
+    },
+      // --------------------------------------------------
+  getLocalArticles(cb) {
+    var that = this;
+    if (this.globalData.localArticles) {
+      typeof cb == 'function' && cb(this.globalData.localArticles);
+    } else {
+      wx.getStorage({
+        key: config.storage.articleListKey,
+        success: (res) => {
+          that.globalData.localArticles = res.data;
+          typeof cb == 'function' && cb(that.globalData.localArticles);
+        },
+        fail: (error) => {
+          that.globalData.localArticles = {};
+          typeof cb == 'function' && cb(that.globalData.localArticles);
+        }
+      });
+    }
   }
 })
